14864aa6a31b890b3e9ac0c34533b8a3449920a9
Alexander Kormanovsky <kormanowskij@yandex.ru>
2020-01-17 16:24:59 +0300

    refactor; add vars for buttons size;

--- main.py
+++ main.py
@@ -13,16 +13,25 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.button_change_voice = tk.Button(self, text="Изменить голос", width='25', height='10')
+        buttons_width = '25'
+        buttons_height = '10'
+        wrap_length = str(int(buttons_width) * 1)
+
+        self.button_change_voice = tk.Button(self, text="Изменить голос \newef2wb  232323   23232323",
+                                             width=buttons_width, height=buttons_height)
         self.button_change_voice.pack(side=tk.LEFT)
 
-        self.button_quit = tk.Button(self, text="Добавить профиль", width='25', height='10')
+        self.button_quit = tk.Button(self, text="Добавить профиль",
+                                     width=buttons_width, height=buttons_height)
         self.button_quit.pack(side=tk.LEFT)
 
-        self.button_make_new_command = tk.Button(self, text="Добавить команду", width='25', height='10')
+        self.button_make_new_command = tk.Button(self, text="Добавить команду",
+                                                 width=buttons_width, height=buttons_height)
         self.button_make_new_command.pack(side=tk.LEFT)
 
-        self.button_quit = tk.Button(self, text="Выйти из настроек", width='25', height='10')
+        self.button_quit = tk.Button(self, text="Выйти из настроек",
+                                     width=buttons_width, height=buttons_height,
+                                     command=self.master.destroy)  # TODO: change command
         self.button_quit.pack(side=tk.LEFT)
 
 
